33a13b1ede46adf66465118853ce5442b9de8b24
spw <spw.carnakib@gmail.com>
2016-08-20 22:17:15 +0100

    added _dev

---
+++ _dev/a88.go
@@ -0,0 +1,429 @@
+package main
+
+import (
+    "bufio"
+    "encoding/csv"
+    "os"
+    "errors"
+    "fmt"
+    "io"
+    "log"
+    "strings"
+//x     "testing"
+)
+
+//- Keywords
+//- Key,                    low,    high,   type,               scope,  source,     table,  comments__20160617,,
+//- MISSION                 ,1,     1,      Enumerated         ,m,      CAA        ,3,      ,       ,
+//- PARENT_MISSION          ,1,     1,      Enumerated         ,,       CAA        ,3,,,
+//- MISSION_TIME_SPAN       ,1,     1,      ISO_TIME_RANGE     ,m,      CAA        ,3,,,
+
+//- Enum
+//- Keyword,                Definition,     Description,                                            Additional
+//- MISSION,                Cluster,        ,
+//- MISSION,                DoubleStar,     ,
+//- PARENT_MISSION,         Cluster,        Mission or project from which the data was collected:,
+//- MISSION_AGENCY,         ESA,            European Space Agency,
+
+/////////////////////////////////////////////////////////////////////////////// -------------------------------------------------------------------------------
+//
+
+func read_csv(i_path string) chan []string {
+	output := make(chan []string, 1)
+    
+	go func() {
+		defer close(output)
+
+		fi, err := os.Open(i_path)
+		if err != nil {
+            log.Fatal(err)
+			return
+		}
+		defer fi.Close()
+
+        r := csv.NewReader(bufio.NewReader(fi))
+        ln := 0
+        for {
+            record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+            if err != nil {
+                log.Fatal(err)
+                return
+            }
+            
+            if ln > 0 {
+                for i, r := range record {
+                    record[i] = strings.TrimSpace(r)
+                }
+                
+                output <- record
+            }
+            
+            ln++
+        }
+	}()
+
+	return output
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+
+// TODO REMOVE WHEN MERGE TO REMOVE DUPLICATE
+
+type KeyVal struct {
+	key string
+	val []string
+    
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+
+type KeywordType int
+
+const (
+    ENUMERATED KeywordType = iota
+    FORMATTED          
+    INTEGER            
+    ISO_TIME           
+    ISO_TIME_RANGE     
+    NUMERICAL          
+    STRING             
+    TEXT               
+)
+
+func getKeywordType(s string)  (k KeywordType, err error) {
+    
+    s = strings.ToLower(s)
+
+    switch s {
+        case "enumerated" :     k = ENUMERATED
+        case "formatted" :      k = FORMATTED
+        case "integer" :        k = INTEGER
+        case "iso_time" :       k = ISO_TIME
+        case "iso_time_range" : k = ISO_TIME_RANGE
+        case "numerical" :      k = NUMERICAL
+        case "string" :         k = STRING
+        case "text" :           k = TEXT    
+        default : 
+            err = errors.New("Error: unknown Keyword value type: [" + s + "]")
+    }
+    
+    return
+}
+
+// ------------------------------------
+
+type KeywordData struct {
+    lo              string
+    hi              string
+    keyword_type    KeywordType
+    
+    scope           string
+    table           string
+    comment         string
+}
+
+func (kw *KeywordData) is_range(lo, hi string) (bool) {
+    return kw.lo == lo && kw.hi == hi
+}
+
+//x  &KeyVal { key: k, val: []string{ v}}
+func (kw *KeywordData) test_cardinality(kv *KeyVal) (err error) {
+    
+    l := len(kv.val)
+    ls := string(l)
+    switch {
+        case kw.is_range("0", "1") :
+            if l > 1 {
+                err = errors.New("Error: Keyword cardinality(0,1) Actual " + ls + " - " + kv.key)
+            }
+        case kw.is_range("0", "N") :
+            // anything is good!
+        case kw.is_range("1", "1") :
+            if l != 1 {
+                err = errors.New("Error: Keyword cardinality(1,1) Actual " + ls + " - " + kv.key)
+            }
+        case kw.is_range("1", "N") :
+            if l == 0 {
+                err = errors.New("Error: Keyword cardinality(1,N) Actual " + ls + " - " + kv.key)
+            }
+        case kw.is_range("0*", "1*") :
+            if l == 0 {
+                err = errors.New("Error: Keyword cardinality(0*,1*) Actual " + ls + " - " + kv.key)
+            }
+        case kw.is_range("0*", "N*") :
+            if l == 0 {
+                err = errors.New("Error: Keyword cardinality(0*,N*) Actual " + ls + " - " + kv.key)
+            }
+            
+        default :
+    }
+
+    return 
+}
+
+
+// TODO NEED TO MOVE the FUNC !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+func test_enumerated(v string) (err error) {
+    return
+}
+
+func test_formatted(v string) (err error) {
+    return
+}
+
+func test_integer(v string) (err error) {
+    return
+}
+
+func test_iso_time(v string) (err error) {
+    return
+}
+
+func test_iso_time_range(v string) (err error) {
+    return
+}
+
+func test_numerical(v string) (err error) {
+    return
+}
+
+func test_string(v string) (err error) {
+    return
+}
+
+func test_text(v string) (err error) {
+    return
+}
+
+
+func (kw *KeywordData) test_value_type(kv *KeyVal) (err error) {
+
+    for _, v := range kv.val {
+        switch kw.keyword_type {
+            case ENUMERATED :
+                // TODO NEED ACCESS TO ENUM DATA
+                err = test_enumerated(v)
+            case FORMATTED :
+                err = test_formatted(v)
+            case INTEGER :
+                err = test_integer(v)
+            case ISO_TIME :
+                err = test_iso_time(v)
+            case ISO_TIME_RANGE :
+                err = test_iso_time_range(v)
+            case NUMERICAL :
+                err = test_numerical(v)
+            case STRING :
+                err = test_string(v)
+            case TEXT :
+                err = test_text(v)
+            default :
+        }
+        
+        if err != nil {
+            return
+        }
+    }
+
+    return 
+}
+
+
+type EnumData struct {
+    description     string
+    additional      string
+}
+
+type MddData struct {
+    m_keywords map[string]KeywordData
+    m_enums map[string]map[string]EnumData
+}
+
+func NewMddData_Base(i_path_keywords, i_path_enums string) *MddData {
+    
+    wd, err := os.Getwd()
+    if err != nil {
+        return nil
+    }
+    
+    fmt.Println("_________________________________")
+    fmt.Println("Current Working Directory = " + wd)
+    
+    
+    mdd_data := &MddData{ m_keywords: make(map[string]KeywordData), 
+                          m_enums: make(map[string]map[string]EnumData) }
+    
+    //x for r := range read_csv("C:\\work.dev.go\\src\\github.com\\caa-dev-apps\\cefmdd_v1\\_mdd-csv\\mdd__20160617.xlsx - Tables.csv") {
+    for r := range read_csv(i_path_keywords) {
+        
+        l_keywordType, err := getKeywordType(r[3])
+        
+        if err != nil {
+            log.Fatal(err)
+            break
+        }
+        
+        //x fmt.Println(r[0], r[1], r[2], r[3])
+        mdd_data.add_keyword(r[0], &KeywordData{ lo : r[1], hi : r[2],  keyword_type : l_keywordType,    scope : r[4],  table : r[4],  comment : r[5] })
+    }
+    
+    //x fmt.Println("\n________________________________________\n")
+    //x for r := range read_csv("C:\\work.dev.go\\src\\github.com\\caa-dev-apps\\cefmdd_v1\\_mdd-csv\\mdd__20160617.xlsx - Enums.csv") {
+    for r := range read_csv(i_path_enums) {
+        //x fmt.Println(r[0], r[1])
+        mdd_data.add_enum(r[0], r[1], &EnumData{description: r[2], additional: r[2]})
+    }
+
+    return mdd_data
+    //x mdd_data.dump()
+}
+
+
+func (d *MddData) add_enum(i_keyword, 
+                           i_definition string, 
+                           i_enum_data *EnumData) (err error) {
+
+    _, present := d.m_enums[i_keyword]
+    if present == false {
+        d.m_enums[i_keyword] = make(map[string]EnumData)
+    }
+
+    _, present = d.m_enums[i_keyword][i_definition]
+    if present == true {
+        return errors.New("Error: add_enum: keyword[definition] exists")
+    } else {
+        d.m_enums[i_keyword][i_definition] = *i_enum_data
+    }
+    
+    return
+}
+
+func (d *MddData) add_keyword(i_keyword string, 
+                              i_kw_data *KeywordData) (err error) {
+
+    _, present := d.m_keywords[i_keyword]
+    if present == false {
+        d.m_keywords[i_keyword] = *i_kw_data
+    } else {
+        return errors.New("Error: add_keyword: keyword exists")
+    }
+    
+    return
+}
+
+func (d *MddData) dump() {
+    fmt.Println(d.m_keywords)
+    fmt.Println(d.m_enums)
+}
+
+/////////////////////////////////////////////////////////////////////////////// -------------------------------------------------------------------------------
+//
+
+//-  TODO
+//-     tests
+//-         lo - hi
+//-         type 
+
+//-         Enumerated KeywordType = iota
+//-         Formatted          
+//-         Integer            
+//-         ISO_TIME           
+//-         ISO_TIME_RANGE     
+//-         Numerical          
+//-         String             
+//-         Text    
+
+// keyword 
+
+//- Key,                    low,    high,   type,               scope,  source,     table,  comments__20160617,,
+//- MISSION                 ,1,     1,      Enumerated         ,m,      CAA        ,3,      ,       ,
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+//- Enum
+//- Keyword,                Definition,     Description,                                            Additional
+
+
+func (d *MddData) test_input(kv *KeyVal) (err error) {
+
+    //- k := kv.k
+    //- v := kv.v [...]
+
+    //- keyword_presence
+    //- cardinality (lo, hi)
+    //- keyword_type
+    
+    l_kw_data, present := d.m_keywords[kv.key]
+    if present == false {
+        return errors.New("Error: keyword not found: " + kv.key)
+    } 
+    
+    err = l_kw_data.test_cardinality(kv)
+    if err != nil {
+        return 
+    }
+    
+    err = l_kw_data.test_value_type(kv)
+    if err != nil {
+        return 
+    }
+    
+    return
+}
+
+func (d *MddData) test_input_kv1(k, 
+                                 v string) (err error) {
+    
+    kv := &KeyVal{ key: k, val: []string{v}}
+    err = d.test_input(kv)
+    
+    return 
+}
+
+func (d *MddData) test_input_kv2(k string, 
+                                 v []string) (err error) {
+    
+    kv := &KeyVal{ key: k, val: v}
+    err = d.test_input(kv)
+    
+    return 
+}
+
+
+func NewMddData() *MddData {
+   return NewMddData_Base("C:\\work.dev.go\\src\\github.com\\caa-dev-apps\\cefmdd_v1\\_mdd-csv\\mdd__20160617.xlsx - Tables.csv", 
+                          "C:\\work.dev.go\\src\\github.com\\caa-dev-apps\\cefmdd_v1\\_mdd-csv\\mdd__20160617.xlsx - Enums.csv")
+     
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+
+func main() {
+    
+    fmt.Println("Hello, World!")
+    
+    mdd_data := NewMddData()
+    
+    //x mdd_data.dump()
+    kv := &KeyVal{ key: "LOGICAL_FILE_ID", val: []string{"SOME_FILE_ID"}}
+
+    err := mdd_data.test_input(kv)
+    if err != nil {
+        log.Print(err)        
+    }
+    
+    err = mdd_data.test_input_kv1("LOGICAL_FILE_ID_1", "SOME_FILE_ID")
+    if err != nil {
+        log.Print(err)        
+    }
+    
+    err = mdd_data.test_input_kv2("LOGICAL_FILE_ID_2", []string{"SOME_FILE_ID"})
+    if err != nil {
+        log.Print(err)        
+    }
+    
+}
---
+++ _dev/a88_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+//x     "bufio"
+//x     "encoding/csv"
+//x     "os"
+//x     "errors"
+     "fmt"
+//x     "io"
+     "log"
+//x     "strings"
+     "testing"
+//x     "github.com/caa-dev-apps/cefmdd_v1/_dev"    
+)
+
+//- Keywords
+//- Key,                    low,    high,   type,               scope,  source,     table,  comments__20160617,,
+//- MISSION                 ,1,     1,      Enumerated         ,m,      CAA        ,3,      ,       ,
+//- PARENT_MISSION          ,1,     1,      Enumerated         ,,       CAA        ,3,,,
+//- MISSION_TIME_SPAN       ,1,     1,      ISO_TIME_RANGE     ,m,      CAA        ,3,,,
+
+//- Enum
+//- Keyword,                Definition,     Description,                                            Additional
+//- MISSION,                Cluster,        ,
+//- MISSION,                DoubleStar,     ,
+//- PARENT_MISSION,         Cluster,        Mission or project from which the data was collected:,
+//- MISSION_AGENCY,         ESA,            European Space Agency,
+
+/////////////////////////////////////////////////////////////////////////////// -------------------------------------------------------------------------------
+//
+
+func Test0001(t *testing.T) {
+    if testing.Short() {
+        t.Skip("skipping test in short mode.")
+    }
+    
+    fmt.Println("Hello, World!")
+    
+    mdd_data := NewMddData()
+                           
+    err = mdd_data.test_input_kv1("LOGICAL_FILE_ID_1", "SOME_FILE_ID")
+    if err != nil {
+        log.Print(err)        
+    }
+    
+    err = mdd_data.test_input_kv2("LOGICAL_FILE_ID_2", []string{"SOME_FILE_ID"})
+    if err != nil {
+        log.Print(err)        
+    }
+    
+}
+
+
+
