684bc77c3184412a4215531bf9e72cfd931885d5
claran01 <spw.carnakib@gmail.com>
2016-12-29 12:58:29 +0000

    cef reader data updates

--- cef_reader.go
+++ cef_reader.go
@@ -2,9 +2,7 @@ package main
 
 import (
     "fmt"
-	//x "github.com/caa-dev-apps/cefmdd_v1/header"    
 	"github.com/caa-dev-apps/cefmdd_v1/readers"    
-	"github.com/caa-dev-apps/cefmdd_v1/utils"    
 )
 
 
@@ -15,39 +13,15 @@ func ReadCef(args *utils.CefArgs) (err error) {
 
 	fmt.Println("....", l_path)
 
-	//x _, err = ReadHeader(args, l_lines)
 	l_header, err := ReadHeader(args, l_lines)
 	if err != nil {
 		return
 	}
 
 	//x fmt.Printf("%#v", l_header)
-	err = ReadData(args, l_lines)
+	err = ReadData(l_header, l_lines)
 
 	//x l_header.Dump()
-
-	vs := l_header.Vars().List()
-
-//x 	for ix, v := range vs {
-//x 		fmt.Println(ix)		
-//x 		fmt.Println(v)		
-//x 		fmt.Println("\n\n")		
-//x 	}
-
-	//x vs,			 []Attrs
-	//x Attrs v, 	map[string][]string
-	for ix, v := range vs {
-		fmt.Println(ix)		
-
-		for k1, v1 := range v.Map() {
-			fmt.Println(k1, "\t", v1)		
-		}
-
-		//x fmt.Println(v)		
-		fmt.Println("\n\n")		
-	}
-
-	//x fmt.Println(vs)
 
 	return
 }
--- cef_reader_data.go
+++ cef_reader_data.go
@@ -2,8 +2,9 @@ package main
 
 import (
     "fmt"
+    "github.com/caa-dev-apps/cefmdd_v1/header"
     "github.com/caa-dev-apps/cefmdd_v1/readers"
-	"github.com/caa-dev-apps/cefmdd_v1/utils"    
+//x 	"github.com/caa-dev-apps/cefmdd_v1/utils"    
 )
 
 
@@ -25,7 +26,42 @@ import (
 //! 		- Check Time/Date stamp 
 
 
-func ReadData(args *utils.CefArgs,
+type Cell struct {
+	Variable_ix 	int
+	Cell_ix 		int
+	Variable_type 	int
+}
+
+func NewCellsArray(i_header header.CefHeaderData) (r_cells []Cell) {
+
+	vs := i_header.Vars().List()
+
+	for ix, v := range vs {
+		fmt.Println(ix)		
+
+		for k1, v1 := range v.Map() {
+			fmt.Println(k1, "\t", v1)	
+
+			// TODO move to []struct {
+			// 	Variable_Name string
+			// 	Value_Type string
+			// 	Sizes_total n0 x n1 x n2...
+			// }
+			// 
+		}
+
+		//x fmt.Println(v)		
+		fmt.Println("\n\n")		
+	}
+
+	//x fmt.Println(vs)
+
+	return
+}
+
+
+
+func ReadData(i_header header.CefHeaderData,
 			  i_lines chan readers.Line) (r_err error) {
 
 	// dev data line reader
@@ -39,6 +75,8 @@ func ReadData(args *utils.CefArgs,
 
 		ix++
 	}
+
+	_ = NewCellsArray(i_header)
 
 	return
 }
