aa378a17d4b911a58a1a04a608d89354dc62358f
claran01 <spw.carnakib@gmail.com>
2017-02-09 22:15:56 +0000

    v1.0.8

--- cmd/cefmdd_v1/app.go
+++ cmd/cefmdd_v1/app.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-    diag.Info(diag.Yellow("cefmdd v1.0.7, (08 Feb 2017)"))
+    diag.Info(diag.Yellow("cefmdd v1.0.8, (09 Feb 2017)"))
 
 	s_args, err := utils.NewCefArgs()
 	if err != nil {
