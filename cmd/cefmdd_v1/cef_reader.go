d61ccacc9e88c526c94c70bfc5b08da721977312
claran01 <claran01@users.noreply.github.com>
2024-02-12 13:51:12 +0000

    Update cef_reader.go

--- cmd/cefmdd_v1/cef_reader.go
+++ cmd/cefmdd_v1/cef_reader.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+//	"fmt"
 	"github.com/caa-dev-apps/cefmdd_v1/pkg/readers"    
 	"github.com/caa-dev-apps/cefmdd_v1/pkg/utils"    
 )
@@ -10,10 +11,9 @@ func ReadCef(args *utils.CefArgs) (err error) {
 
 	l_path := args.GetCefPath()
 	l_lines := readers.EachLine(l_path)
-
 	l_header, err := ReadHeader(args, l_lines)
 	if err != nil {
-		return
+		return err
 	}
 
 	err = ReadData(l_header, l_lines)
