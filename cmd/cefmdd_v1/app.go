ecda68ffdacdd1b56c237d805aeaff8b84f10ba2
claran01 <claran01@users.noreply.github.com>
2024-02-12 13:49:02 +0000

    Update app.go

--- cmd/cefmdd_v1/app.go
+++ cmd/cefmdd_v1/app.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"os"
 
+	"os"
     "github.com/caa-dev-apps/cefmdd_v1/pkg/diag"
     "github.com/caa-dev-apps/cefmdd_v1/pkg/utils"
 )
 
 func main() {
 
-    diag.Info(diag.Yellow("cefmdd v1.1.1, (29 July 2017)"))
+//20200120 added check for matched quotes
+//20200123 added check that CC files match CONTEXT OBS
+//20200123 added check that attributes must be unique within param
+//20200207 added check for EOR marker on last data line
+//20200207 updated observatory from context to cluster-context
+//20210615    diag.Info(diag.Yellow("cefmdd v1.2.5 BETA, (20200702)"))
+//20230929    diag.Info(diag.Yellow("cefmdd v1.2.7 BETA, (20230301)"))
 
+        diag.Info(diag.Yellow("cefmdd v1.2.7b, (20230929)"))
 	s_args, err := utils.NewCefArgs()
 	if err != nil {
 		os.Exit(-1)
@@ -24,5 +31,5 @@ func main() {
 
     diag.Info(diag.Yellow("CAA Rocks!\n"))
 
-    return 0
+    return
 }
